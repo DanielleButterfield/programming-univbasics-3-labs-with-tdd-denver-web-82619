97497cdecb7dd6d2d6bc2949a83329f0df817293
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-19 01:52:28 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
 # Add your variables here
+var first_number = 13;
+var second_number = 7;
+
+var sum = first_number+second_number;
+
+var difference = first_number-second_number;
+
+var product = first_number*second_number;
+
+var quotient = first_number/second_number;
